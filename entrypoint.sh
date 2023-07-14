5fd74ee14d28835d69acde34c0c51aa5e6f7e6b9
Justin B <justin@penguintech.io>
2023-07-14 21:10:38 +0000

    multiple bug fixes and cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 ansible-playbook entrypoint.yml  -c local --tags run
 /opt/waddleperf3/pyweb.sh &
-/opt/manager/bins/server.sh
-
-#echo "Sleepy time!"
-#/usr/bin/sleep infinity
+sed -i 's/ \{2,\}/ /g' /opt/waddleperf3/iperf3-server
+sed -i 's/ \{2,\}/ /g' /opt/waddleperf3/iperf3-client
+if [ "$IPERF_SERVER_ENABLE" == "1" ]
+then
+    /opt/manager/bins/server.sh
+else
+    echo "Sleepy time in client mode!"
+    /usr/bin/sleep infinity
+fi
