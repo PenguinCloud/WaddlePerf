345ae2fe09f009e1b7845ea0b9b5be917f501832
Justin B <justin@penguintech.io>
2023-07-14 23:15:52 +0000

    DOnt need pyweb if client

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 ansible-playbook entrypoint.yml  -c local --tags run
-cd /opt/waddleperf3
-/opt/waddleperf3/pyweb.sh &
 sed -i 's/ \{2,\}/ /g' /opt/waddleperf3/iperf3-server
 sed -i 's/ \{2,\}/ /g' /opt/waddleperf3/iperf3-client
 if [ "$IPERF_SERVER_ENABLE" == "1" ]
 then
+    /opt/waddleperf3/pyweb.sh &
     /opt/manager/bins/server.sh
 else
     echo "Sleepy time in client mode!"
