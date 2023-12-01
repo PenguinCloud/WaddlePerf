8a970f982669f5fa767d3cba6c774ca0a964062a
Justin B <justin@penguintech.io>
2023-12-01 05:52:18 +0000

    Adding in NGINX to replace simple Python server

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ sed -i 's/ \{2,\}/ /g' /opt/waddleperf3/iperf3-server
 sed -i 's/ \{2,\}/ /g' /opt/waddleperf3/iperf3-client
 if [ "$IPERF_SERVER_ENABLED" == "1" ]
 then
-    /opt/waddleperf3/pyweb.sh &
+    echo "Starting NGINX and Iperf servers"
+    nohup /usr/sbin/nginx &
     /opt/manager/bins/server.sh
 else
     echo "Sleepy time in client mode!"
