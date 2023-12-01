3054e8b22ea0f7a6de36ad1b7edb88160e9a5dac
Justin B <justin@penguintech.io>
2023-12-01 05:25:58 +0000

    initial batches of changes and new features!

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 ansible-playbook entrypoint.yml  -c local --tags run
 sed -i 's/ \{2,\}/ /g' /opt/waddleperf3/iperf3-server
 sed -i 's/ \{2,\}/ /g' /opt/waddleperf3/iperf3-client
-if [ "$IPERF_SERVER_ENABLE" == "1" ]
+if [ "$IPERF_SERVER_ENABLED" == "1" ]
 then
     /opt/waddleperf3/pyweb.sh &
     /opt/manager/bins/server.sh
