66135bdcb7ccee6a4b15339ec0010f48aa910521
Justin B <justin@penguintech.io>
2024-11-12 23:26:34 +0000

    Working out some bugs as this was an early / partial / engineering release

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ansible-playbook entrypoint.yml  -c local --tags run
-sed -i 's/ \{2,\}/ /g' /opt/waddleperf3/iperf3-server
-sed -i 's/ \{2,\}/ /g' /opt/waddleperf3/iperf3-client
+ansible-playbook entrypoint.yml  -c local --tags run 
+sed -i 's/ \{2,\}/ /g' /opt/waddleperf/iperf3-server
+sed -i 's/ \{2,\}/ /g' /opt/waddleperf/iperf3-client
 if [ "$IPERF_SERVER_ENABLED" == "1" ]
 then
     echo "Starting NGINX and Iperf servers"
