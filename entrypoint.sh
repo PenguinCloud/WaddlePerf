4a8898407ef03f2258308bc0f81cec4962f7af4d
Justin B <justin@penguintech.io>
2024-11-13 18:42:29 +0000

    fix iperf server flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ ansible-playbook entrypoint.yml  -c local --tags run
 sed -i 's/ \{2,\}/ /g' /opt/waddleperf/iperf3-server
 sed -i 's/ \{2,\}/ /g' /opt/waddleperf/iperf3-client
 /usr/sbin/nginx &
-if [ "$IPERF_SERVER_ENABLED" == "1" ]
+if [ "$IPERF_SERVER_ENABLED" == "true" ]
 then
     echo "Starting NGINX and Iperf servers"
     /opt/manager/bins/server.sh
