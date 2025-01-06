5cb37447291ca71aec5106aedd3613588fcbe9e5
Justin B <justin@penguintech.io>
2025-01-06 17:55:11 +0000

    Adding iperf json output parsing

--- server/bins/db.py
+++ server/bins/db.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from pydal import DAL, Field, IS_IPADDRESS, IS_ALPHANUMERIC, IS_LENGTH
+from pydal import DAL, Field, IS_IPADDRESS, IS_ALPHANUMERIC, IS_LENGTH, IS_IN_SET
 from dataclasses import dataclass
 
 
@@ -20,6 +20,7 @@ class perfInfo:
     testType: str
     testDstHost: str
     testSrcHost: str
+    testHost: str
     avgLatency: float
     avgThroughput: float
     avgJitter: float
@@ -29,10 +30,14 @@ class dbConnect:
     def __init__(self, dbInfo):
         self.db = DAL(dbInfo.DbType+'://'+dbInfo.dbUser+':'+dbInfo.dbPass+'@'+dbInfo.dbHost+':'+dbInfo.dbPort+'/'+dbInfo.dbName)
         self.db.define_table('testResults', 
-                             Field('testName', 'string', required=True, requires = IS_ALPHANUMERIC()), 
-                             Field('testType', 'string', required=True, requires = IS_ALPHANUMERIC()), 
-                             Field('testDstHost', 'string', requires = IS_IPADDRESS()), 
-                             Field('testSrcHost', 'string', requires = IS_IPADDRESS()), 
+                             Field('testName', 'string', required=True, requires = IS_ALPHANUMERIC()),
+                             Field('testType', 'string', required=True, requires = IS_IN_SET(['sshping', 'iperf', 'httptrace', 
+                                                                                              'pping-dns', 'pping-quic', 'pping-tcp', 
+                                                                                              'pping-tls', 'pping-http',
+                                                                                              'traceroute','mtr', 'ping'])), 
+                             Field('testHost', 'string', required=True, requires = IS_ALPHANUMERIC(), label='hostname which the test was for'), 
+                             Field('testDstHost', 'string', requires = IS_IPADDRESS(), label='Acting Server host for testing'), 
+                             Field('testSrcHost', 'string', requires = IS_IPADDRESS(), label='Client host for testing'), 
                              Field('avgLatency', 'float', requires=IS_LENGTH(3, 12)), 
                              Field('avgThroughput', 'float', requires=IS_LENGTH(3, 12)), 
                              Field('avgJitter', 'float', requires=IS_LENGTH(3, 12)), 
@@ -65,6 +70,14 @@ class logParser():
     def iperf(self):
         with open(self.logFile, 'r') as file:
             perfData= json.load(file)
-            
+            file.close
+        # TODO: CHange test name to be something onboarded as like a description later
+        self.result.testName = "iperf"
+        self.result.testType = "iperf"
+        # TODO: Change this to be more dynamic later from request
+        self.result.testHost = "nohost.local"
+        self.result.testDstHost = perfData['server_ip']
+        self.result.testSrcHost = perfData['client_ip']
+    
                     
         return self.result
