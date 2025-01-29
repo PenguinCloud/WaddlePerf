7cb340ef2a0db83fc27c84d541080acec5368eb7
Justin B <justin@penguintech.io>
2025-01-29 04:54:32 +0000

    Client is building and build tests are suceeding. Moving to server

--- client/thin-installers/mac-thininstall.sh
+++ client/thin-installers/mac-thininstall.sh
@@ -3,7 +3,17 @@ $PPING_REPO = "wzv5/pping"
 $VERSION = curl --silent "https://api.github.com/repos/wzv5/pping/releases/latest" | jq -r .tag_name | cut -d "v" -f 2
 
 echo "Pulling latest version $VERSION"
-
+if ! command -v brew &> /dev/null
+then
+  echo "Homebrew is required and it is not installed. Would you like to install it now? (y/n)"
+  read -r response
+  if [[ "$response" == "y" ]]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  else
+    echo "Please install Homebrew first."
+    exit 1
+  fi
+fi
 brew install iperf3
 brew install speedtest-cli
 brew install httping
