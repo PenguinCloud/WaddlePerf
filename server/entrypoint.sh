b85021a57d45be847d2c096aaa6b2d784e2e9326
Justin B <justin@penguintech.io>
2025-02-04 16:01:31 +0000

    Fix github actions to split between server and client images!

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 ansible-playbook entrypoint.yml  -c local --tags run 
 sed -i 's/ \{2,\}/ /g' /opt/waddleperf/iperf3-server
-sed -i 's/ \{2,\}/ /g' /opt/waddleperf/iperf3-client
-/usr/sbin/nginx &
-if [ "$IPERF_SERVER_ENABLED" == "true" ]
-then
-    echo "Starting NGINX and Iperf servers"
-    /opt/manager/bins/server.sh
-else
-    echo "Sleepy time in client mode!"
-    /usr/bin/sleep infinity
-fi
+echo "Starting NGINX and Iperf servers"
+nohup /usr/local/bin/iperf3-server &
+/usr/sbin/nginx -g 'daemon off;'
