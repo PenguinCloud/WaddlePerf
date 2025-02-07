b5e9ee13896d6ac120bc8180d79d817cc6033b22
Justin B <justin@penguintech.io>
2025-02-07 19:04:47 +0000

    Adding GeoIP foundations

--- server/web/userstats.py
+++ server/web/userstats.py
@@ -5,8 +5,17 @@ app = Flask(__name__)
 
 @app.route('/ip', methods=['GET'])
 def ip():
-    client_ip = request.remote_addr
-    return jsonify({'ip': client_ip})
+    output = {"RequestClientIP": request.remote_addr, "X-Forwarded-For": request.headers.get('X-Forwarded-For'), "X-Real-IP": request.headers.get('X-Real-IP')}
+    if 'X-Forwarded-For' in request.headers:
+        output["IP"] = request.headers['X-Forwarded-For'].split(',')[0].strip()
+        output["Method"] = 'X-Forwarded-For'
+    elif 'X-Real-IP' in request.headers:
+        output["IP"] = request.headers['X-Real-IP']
+        output["Method"] = 'X-Real-IP'
+    else:
+        output["IP"] = request.environ['REMOTE_ADDR']
+        output["Method"] = 'REMOTE_ADDR'
+    return jsonify(output)
 
 
 if __name__ == '__main__':
