ff0b3afc68da1b76ea18375ff3fda01a839f36e7
Justin B <justin@penguintech.io>
2025-02-11 15:54:04 +0000

    Dropping npm httptrace and using python

--- server/web/userstats.py
+++ server/web/userstats.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 from flask import Flask, request, jsonify
+from geoip2 import database as ipDB
 
 app = Flask(__name__)
+
+
 
 @app.route('/ip', methods=['GET'])
 def ip():
@@ -15,7 +18,22 @@ def ip():
     else:
         output["IP"] = request.environ['REMOTE_ADDR']
         output["Method"] = 'REMOTE_ADDR'
+    # The server will use geolite2 to lookup the user's location based on the IP address
+    output["Location"] = 
     return jsonify(output)
+
+def lookupDetails(ip: str) -> dict:
+    # The server will use geolite2 to lookup the user's location based on the IP address
+    results = {"city": "Unknown", "country": "Unknown"}
+    try:
+        
+        reader = ipDB.Reader('/opt/geoip/GeoLite2-City.mmdb')
+        results["city"] = reader.city(ip).city.name
+        results["country"] = reader.country(ip).country.name
+        
+        return results
+    except:
+        return "Unknown"
 
 
 if __name__ == '__main__':
