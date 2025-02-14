e290a0fdc224917518f707a921bc5c942900ea0e
Justin B <justin@penguintech.io>
2025-02-14 16:05:09 +0000

    multiple bug fixes

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,4 +4,5 @@ sed -i 's/ \{2,\}/ /g' /opt/waddleperf/iperf3-server
 echo "Starting NGINX and Iperf servers"
 nohup /usr/local/bin/iperf3-server &
 nohup /var/www/userstats/userstats.py &
+nohup /usr/local/bin/udpping-server-wrapper &
 /usr/sbin/nginx -g 'daemon off;'
